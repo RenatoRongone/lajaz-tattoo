f5121226a3d2831f1f7eec6b8798f1c9eef376d7
Renato_Rongone <r.rongone17gmail.com>
2024-01-07 16:37:18 +0100

    inserito header e inizio main

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,13 +1,13 @@
 <nav class="navbar navbar-expand-lg bg-dark">
-    <div class="container-fluid">
+    <div class="container-fluid ">
         <a class="navbar-brand textWhite ps-5" href="{{route('welcome')}}">
             <img src="/media/jas.png" alt="Logo" class="logo-custom">
             {{-- logo da sistemare --}}
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler btn-nav" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class=" justify-content-end pe-5 ">
+        <div class=" justify-content-end">
             <div class="collapse navbar-collapse " id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
                     <li class="nav-item">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,25 @@
 <x-layout>
-    
+    <header class="container-fluid vh-100 ">
+        <div class="row h-100">
+            <div class="col-12 d-flex align-items-center justify-content-center  header">
+                <h1 class="display-1 textWhite ">
+                    La Jaz-Tattoo
+                </h1>
+            </div>
+        </div>
+    </header>
+    <main class="container vh-100 mt-2">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="text-center my-3">
+                    I miei ultimi lavori
+                </h2>
+            </div>
+            <div class="col-12 col-md-3 justify-content-center d-flex">
+                <div class="card">
+                    <img class="img-custom" src="https://picsum.photos/id/237/200/300" alt="image tattoo">
+                </div>
+            </div>
+        </div>
+    </main>
 </x-layout>
