149879dcbaddbe45c91983150a25fa53b0900bb9
Renato_Rongone <r.rongone17gmail.com>
2024-01-09 12:36:08 +0100

    card inserita

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,9 @@
 <x-layout>
     <header class="container-fluid vh-100 ">
-        <div class="row h-100">
-            <div class="col-12 d-flex align-items-center justify-content-center  header">
-                <h1 class="display-1 textWhite ">
-                    La Jaz-Tattoo
+        <div class="row h-100"> 
+            <div class="col-12 d-flex align-items-center justify-content-center header">
+                <h1 class="display-1 textWhite" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+                    Tattoo-Studio
                 </h1>
             </div>
         </div>
@@ -16,8 +16,15 @@
                 </h2>
             </div>
             <div class="col-12 col-md-3 justify-content-center d-flex">
-                <div class="card">
-                    <img class="img-custom" src="https://picsum.photos/id/237/200/300" alt="image tattoo">
+                <div class = "card">
+                    <img src="https://images.unsplash.com/photo-1656618020911-1c7a937175fd?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTc1MzQyNTE&ixlib=rb-1.2.1&q=80" alt="Image tattoo">
+                    <div class="card-content">
+                        <h2 class="textWhite">
+                            Titolo
+                        </h2>
+                    </div>
+                    <a href="#" class="button textWhite">
+                    </a>
                 </div>
             </div>
         </div>
