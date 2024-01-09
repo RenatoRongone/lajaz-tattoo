25dd290b77fd640e810795a39e1b3494e3339b3f
Renato_Rongone <r.rongone17gmail.com>
2024-01-09 19:33:57 +0100

    sistemata pagina artista responsive

--- resources/views/artistpage.blade.php
+++ resources/views/artistpage.blade.php
@@ -6,15 +6,15 @@
                         Chi è l'Artista?
                     </h1>
                 </div>
-            <div class="row my-5" data-aos="fade-up" data-aos-anchor-placement="top-center">
-                <div class="col-4">
+            <div class="row my-5 justify-content-center" data-aos="fade-up" data-aos-anchor-placement="top-center">
+                <div class="col-12 col-md-4 d-flex justify-content-center ">
                     <img class="img-custom" src="/media/artist.jpg" alt="atist photo">
                 </div>
-                <div class="col-8">
+                <div class="col-12 col-md-8 mt-3 mt-md-0">
                     <h2 class="text-center">
                         Mi presento
                     </h2>
-                    <p>
+                    <p  class="text-center">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et voluptatum quaerat veritatis id eos sapiente perferendis fuga soluta nesciunt. Commodi praesentium qui, sint voluptas consectetur sunt aliquam quisquam ipsam enim itaque! Sunt facilis corporis voluptate dolore nostrum pariatur vero ducimus quos necessitatibus nobis officiis similique rerum maxime sint totam voluptatum vitae quod iusto velit sequi, reprehenderit illum. Odio, id quas cupiditate architecto doloribus nisi eveniet, sunt officia sapiente non corrupti, omnis earum quis velit nam tempore impedit aliquam? Autem, iure?
                     </p>
                 </div>
