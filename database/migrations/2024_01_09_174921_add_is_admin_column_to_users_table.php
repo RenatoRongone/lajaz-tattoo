083702e53a3a346de9c5830109bbb5ecede13960
Renato_Rongone <r.rongone17gmail.com>
2024-01-16 16:44:28 +0100

    problema persiste anche con il cambio del db

--- database/migrations/2024_01_09_174921_add_is_admin_column_to_users_table.php
+++ database/migrations/2024_01_09_174921_add_is_admin_column_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('user_id')->nullable()->after('name');
+            $table->boolean('is_admin')->nullable()->after('name');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('user_id');
+            $table->dropColumn('is_admin');
         });
     }
 };
--- database/migrations/2024_01_15_145740_create_projects_table.php
+++ database/migrations/2024_01_15_145740_create_projects_table.php
@@ -13,9 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('title');
             $table->string('description')->nullable();
-            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
